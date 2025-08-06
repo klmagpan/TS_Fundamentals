0a633be0e0e7ae7d82a9658562eace8b60ea110f
klmagpan <klmagpan@ucsc.edu>
2025-08-06 13:45:32 -0700

    Remove moved files and add module/configuration folders

--- decorators.ts
+++
@@ -1,9 +0,0 @@
-// export function logMethodInfo(origMethod: any, _context: ClassMethodDecorator Context) {
-// 	function replacementMethod(this:any, ... args:any[]) {
-// 		console.log(`Decorated construst: ${_context.kind}`);
-// 		console.log(`Decorated construct name: ${_context.name as string}`);
-// 		const result = origMethod.call(this, ...args);
-// 		return result;
-// 	}
-// 	return replacementMethod;
-// }
